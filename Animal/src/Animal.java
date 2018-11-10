39ccf3db14f8b9fb02be4b725649e383329e1a70
hakason <dehanhakason@gmail.com>
2018-11-10 15:59:31 +0100

    TEST

---
+++ Animal/src/Animal.java
@@ -0,0 +1,16 @@
+
+public class Animal {
+	
+	 public static void main(String[] args) {
+	        Animal cat = new Animal();
+	        cat.setName("boby");
+
+	        Animal dog = new Animal();
+	        dog.setName("happy");
+
+	        System.out.println(cat.name);
+	        System.out.println(dog.name);
+	    }
+
+
+}
